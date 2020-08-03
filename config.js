44b9cac2d267c523a1736d808dc37a8a3b256c51
asesinosdeinsectos049 <69062579+asesinosdeinsectos049@users.noreply.github.com>
2020-08-03 11:44:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATOMLkNkqGwP2DudWGRMXbBRQ0b8nAi_Q",
+    authDomain: "form-892f0.firebaseapp.com",
+    databaseURL: "https://form-892f0.firebaseio.com",
+    projectId: "form-892f0",
+    storageBucket: "form-892f0.appspot.com",
+    messagingSenderId: "1088370714909",
+    appId: "1:1088370714909:web:852dd1e95a232b300699f4",
+    measurementId: "G-DX1DNJN1V6"
+  };
+        firebase.initializeApp(firebaseConfig);
